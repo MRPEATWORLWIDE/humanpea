ce5151add5ed0de2016f2c1254708e989904f1b8
MRPEATWORLWIDE <josh@asap.london>
2025-10-25 13:21:08 +0100

    Update and rename tailwind.config.ts to tailwind.config.js

--- tailwind.config.ts
+++
@@ -1,7 +0,0 @@
-import type { Config } from "tailwindcss";
-const config: Config = {
-  content: ["./app/**/*.{ts,tsx}", "./styles/**/*.{css}"],
-  theme: { extend: {} },
-  plugins: []
-};
-export default config;
