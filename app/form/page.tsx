352f27fdd7e360e8a2b7723bf7e1e422986447bb
MRPEATWORLWIDE <josh@asap.london>
2025-10-26 09:57:51 +0000

    Create page.tsx

---
+++ app/form/page.tsx
@@ -0,0 +1,18 @@
+export default function FormPage() {
+  return (
+    <main className="min-h-screen bg-white text-black flex items-center justify-center p-4">
+      <iframe
+        src="https://form.jotform.com/252973068946371"
+        title="HumanPea Form"
+        width="100%"
+        height="1200"
+        style={{
+          border: "1px solid #00000015",
+          borderRadius: "12px",
+          maxWidth: "720px",
+          backgroundColor: "#f9f9f9",
+        }}
+      ></iframe>
+    </main>
+  );
+}
